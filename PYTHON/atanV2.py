7d65b352fc01919b1e71030256559904a20bd866
arnolds.slionskis@gmail.com <arnolds.slionskis@gmail.com>
2017-11-22 22:11:41 +0200

    22/11/17_22:11

--- PYTHON/atanV2.py
+++ PYTHON/atanV2.py
@@ -7,6 +7,22 @@ print type(x)
 y=atan(x)
 print "atan(%.2f) = %.2f"%(x,y)
 #--------------------------------------------------------------
+print "          500"
+print "          ---                               K"
+print "         /                        /    2   \ "
+print "  X      |          (2*K)!        |   x    | "
+print "------- * >  ------------------ * |--------| "
+print "   ----  |       2   2K           |     2  | "
+print "  /   2  \   (K!) * 2  *(2*K+1)   \  1+X   / "
+print " \/1+X    ---"
+print "      "
+print "      "
+print "        2        2 "
+print "  (2K-1)        x  "
+print "----------- * ----- "
+print " 2K*(2K+1)       2"
+print "              1+x "
+#--------------------------------------------------------------
 count = 0
 a = 1.
 sum = a
@@ -15,6 +31,10 @@ while count < 500:
 	k = 1.*((2*count-1)**2)/(2*count*(2*count+1))*(x**2)/(1+(x**2))
 	a = a * k
 	sum = sum + a
+	if count == 499:
+		print "sum(499) = %.2f"%(sum)
 z = (x/(sqrt(1+(x**2))))
 y = z * sum
 print "my_atan(%.2f) = %.2f"%(x,y)
+print "sum = %.2f"%(sum)
+print "a = %.2f"%(a)
